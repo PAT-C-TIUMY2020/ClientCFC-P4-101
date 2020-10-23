1e69e12b1f3cc14038a5f97c773cb1f41780dc73
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2020-10-23 18:34:22 +0700

    Add project files.

---
+++ ClientCFC-P4-101/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceMtk_P1_101;
+using System.ServiceModel;
+
+namespace ClientCFC_P4_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1910");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+            Koordinat objk = new Koordinat();
+        }
+    }
+}
